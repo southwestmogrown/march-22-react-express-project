16e7a0cf6388a730050327f51714ca4e19fbfd54
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-06-23 09:29:40 -0500

    tested userAuth

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
 
-router.post('/test', (req, res) => {
-    res.json({ requestBody: req.body });
-});
+
 
 module.exports = router;
