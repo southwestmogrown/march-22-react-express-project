040ae0516232cbe99a346329364bc90998e6dec6
Shane Wilkey <76767429+southwestmogrown@users.noreply.github.com>
2022-06-25 13:15:23 -0500

    signup form styling

--- frontend/src/components/SignupFormModal/SignupForm.js
+++ frontend/src/components/SignupFormModal/SignupForm.js
@@ -28,48 +28,65 @@ function SignupFormPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
-      </ul>
-      <label>
-        Email
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Username
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Confirm Password
-        <input
-          type="password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-      </label>
-      <button type="submit">Sign Up</button>
-    </form>
+    <div className="signup-form-container">
+
+        <form className='signup-form' onSubmit={handleSubmit}>
+            <ul>
+                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+            </ul>
+            <div className="form-field">
+                <label htmlFor="email">
+                    Email:
+                </label>
+                <input
+                className="email"
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+            </div>
+            <div className="form-field">
+                <label htmlFor="username">
+                    Username:
+                </label>
+                <input
+                className="username"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                />
+            </div>
+            <div className="form-field">
+                <label htmlFor="password">
+                    Password:
+                </label>
+                <input
+                className="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+            </div>
+            <div className="form-field">
+                <label htmlFor="confirm-password">
+                    Confirm Password:
+                </label>
+                <input
+                className="confirm-password"
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+                />
+            </div>
+            <div className="form-field">
+                <button type="submit">Sign Up</button>
+            </div>
+        </form>
+    </div>
   );
 }
 
